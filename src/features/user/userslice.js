fe3b1c658fa2310c1622c6c554461493f641a6bf
amparo1206 <amparo.frontend@gmail.com>
2022-03-21 19:43:22 +0100

    adding register to userSlice

--- src/features/user/userslice.js
+++ src/features/user/userslice.js
@@ -1,2 +1,32 @@
 import userService from "./userService";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+export const userSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.fulfilled, (state, action) => {
+                state.isSucscess = true;
+                state.message = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isError = true;
+                state.message = action.payload
+            })
+    }
+});
+
+export const register = createAsyncThunk("auth/register", async (user, thunkApi) => {
+    try {
+        return await userService.register(user)
+    } catch (error) {
+        const message = error.response.data[0].message;
+        return thunkApi.rejectWithValue(message)
+    }
+})
+
+export default userSlice.reducer;
