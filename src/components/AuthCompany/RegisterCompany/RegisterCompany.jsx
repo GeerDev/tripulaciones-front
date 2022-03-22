38c6bf1fe42d33e42d5c5d10871faf1364178797
Mik3tab <mik3tab@gmail.com>
2022-03-22 17:54:56 +0100

    pasword validation added

--- src/components/AuthCompany/RegisterCompany/RegisterCompany.jsx
+++ src/components/AuthCompany/RegisterCompany/RegisterCompany.jsx
@@ -4,66 +4,65 @@ import { register, reset } from "../../../features/company/companySlice"
 import { useEffect } from "react";
 import { notification } from 'antd';
 
-const RegisterCompany = () =>{
-    const [formData, setFormData] = useState({
-        nameCompany: '',
-        CEO: '',
-        email: '',
-        phone: '',
-        password: '',
-        password2: '',
-    })
-    const { nameCompany, CEO, email, phone, password, password2 } = formData;
+const RegisterCompany = () => {
+  const [formData, setFormData] = useState({
+    nameCompany: '',
+    CEO: '',
+    email: '',
+    phone: '',
+    password: '',
+    password2: '',
+  })
+  const { nameCompany, CEO, email, phone, password, password2 } = formData;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    // const { isError, isSuccess, message } = useSelector((state) => state.auth)
-    // useEffect(() => {
-    //     if (isError) {
-    //         notification.error({ message: "error", description: message });
-    //     }
-    //     if (isSuccess) {
-    //         notification.success({ message: "Welcome", description: message?.message })
-    //     }
-    //     dispatch(reset())
-    // }, [isError, isSuccess, message, dispatch]);
+  // const { isError, isSuccess, message } = useSelector((state) => state.auth)
+  // useEffect(() => {
+  //     if (isError) {
+  //         notification.error({ message: "error", description: message });
+  //     }
+  //     if (isSuccess) {
+  //         notification.success({ message: "Welcome", description: message?.message })
+  //     }
+  //     dispatch(reset())
+  // }, [isError, isSuccess, message, dispatch]);
 
-    const onChange = (e) =>{
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }))
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      return notification.error({
+        message: 'error',
+        description: "Las contraseñas no coinciden",
+      });
+    } else {
+      return dispatch(register(formData));
     }
-
-    const onSubmit = (e) =>{
-        e.preventDefault();
-        return dispatch(register(formData));
-
-        // if(password !== password2){
-        //     return notification.error({
-        //         message: 'error',
-        //         description: "The password does not match",
-        //     });
-        // }else{
-        // }
-}
- return (
+  }
+  return (
     <div>
-        <form onSubmit={onSubmit}>
-            <label>Nombre de la empresa:</label>
-            <input type="text" name="nameCompany" value={nameCompany} onChange={onChange} placeholder="Nombre de la empresa" />
-            <label>CEO:</label>
-            <input type="text" name="CEO" value={CEO} onChange={onChange} placeholder="Nombre del CEO" />
-            <label>Email:</label>
-            <input type="email" name="email" value={email} onChange={onChange} placeholder="Email de la empresa" />
-            <label>Teléfono:</label>
-            <input type="number" name="phone" value={phone} onChange={onChange} placeholder="Teléfono de contacto" maxLength={9} />
-            <label>Contraseña:</label>
-            <input type="password" name="password" value={password} onChange={onChange} placeholder="Contraseña" />
-            <label>Repita la contraseña:</label>
-            <input type="password" name="password2" value={password} onChange={onChange} placeholder="Repita la contraseña"/>
-            <button type="submit">Registrar</button>
-        </form>
+      <form onSubmit={onSubmit}>
+        <label>Nombre de la empresa:</label>
+        <input type="text" name="nameCompany" value={nameCompany} onChange={onChange} placeholder="Nombre de la empresa" />
+        <label>CEO:</label>
+        <input type="text" name="CEO" value={CEO} onChange={onChange} placeholder="Nombre del CEO" />
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={onChange} placeholder="Email de la empresa" />
+        <label>Teléfono:</label>
+        <input type="number" name="phone" value={phone} onChange={onChange} placeholder="Teléfono de contacto" maxLength={9} />
+        <label>Contraseña:</label>
+        <input type="password" name="password" value={password} onChange={onChange} placeholder="Contraseña" />
+        <label>Repita la contraseña:</label>
+        <input type="password" name="password2" value={password2} onChange={onChange} placeholder="Repita la contraseña" />
+        <button type="submit">Registrar</button>
+      </form>
     </div>
   )
 }
