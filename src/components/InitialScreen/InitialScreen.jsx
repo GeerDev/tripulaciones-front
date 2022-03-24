f7a1c2a72679cfca59060cd0fe51c4eacd608664
amparo1206 <amparo.frontend@gmail.com>
2022-03-24 22:24:35 +0100

    styles in initialscreen and in user registration

--- src/components/InitialScreen/InitialScreen.jsx
+++ src/components/InitialScreen/InitialScreen.jsx
@@ -1,8 +1,28 @@
-
+import { Link } from 'react-router-dom';
+import empleado from '../../img/empleado.jpg'
+import empresa from '../../img/empresa.jpg'
+import logo from '../../img/logo.svg'
+import './InitialScreen.scss'
 
 const InitialScreen = () => {
   return (
-    <div>InitialScreen</div>
+    <div className='initial-screen'>
+      <section className='section-initial-screen'>
+      <img src={logo}  />
+        <h1>BIENVENID@</h1>
+        <h3>¿Te atrevés a ser más sostenible?</h3>
+        </section>
+        <ul>
+          <li>
+          <img src={ empresa}  />
+            <Link to='/registercompany'> Empresa </Link>
+        </li>
+        <li>
+            <img src={ empleado}  />
+            <Link to='/registeruser'> Empleado </Link>
+          </li>
+        </ul>
+    </div>
   )
 }
 
