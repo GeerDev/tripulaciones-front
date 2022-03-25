a99b8ba0d672e2dafe01956a27fbe7182a1a5da1
German <gerfersie@gmail.com>
2022-03-25 13:44:15 +0100

    Add Challenge componente & form created

---
+++ src/components/DashboardAdmin/AddChallenge/AddChallenge.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addPost } from '../../../features/post/postSlice'
+
+import '../../../styles/main.scss';
+
+const AddPost = () => {
+
+
+  return (
+    <form onSubmit={onSubmit} className='form card animate__animated animate__fadeIn'>
+        <input type="file" name="imagePost"/>
+        <input type="text" placeholder="Título..." name="title" />
+        <input type="text" placeholder="Descripción..." name="description" />
+    <button type="submit">Añade una publicación</button>
+    </form>
+  )
+}
+
+export default AddPost
