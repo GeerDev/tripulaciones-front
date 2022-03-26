0e84de3e7923fb48fb1b4edc6c96dd5a1f7a5396
miguel Talavera <mik3tab@gmail.com>
2022-03-26 12:20:26 +0100

    guards on all users & profileCompany created

--- src/routers/CompanyRouter.js
+++ src/routers/CompanyRouter.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 
 import DashboardCompany from "../components/DashboardCompany/DashboardCompany";
-
+import CompanyZone from "../guards/CompanyZone";
 export const CompanyRouter = () => {
     return (
             <Routes>
                 <Route path="/" element={< DashboardCompany/>} />
+                <Route path="/profile" />
             </Routes>
             )
 }
