a199a8e22ae0235dca484f3d1f6dcbf629db1f3c
amparo1206 <amparo.frontend@gmail.com>
2022-03-27 17:02:38 +0200

    sidebar changes

---
+++ src/components/DashboardCompany/EditCompany/EditCompany.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditCompany = () => {
+  return (
+    <div>EditCompany</div>
+  )
+}
+
+export default EditCompany
--- src/features/comment/commentSlice.js
+++ src/features/comment/commentSlice.js
@@ -11,13 +11,45 @@ export const commentSlice = createSlice({
     reducers: {
     },
     extraReducers: (builder) => {
-        builder
-            .addCase(createComment.fulfilled, (state, action) => {
-                state.comments = [action.payload, ...state.comments]
+         builder
+            .addCase(getComments.fulfilled, (state, action) => {
+                state.comments = action.payload;
             })
+            .addCase(createComment.fulfilled, (state, action) => {
+                state.comments = [...state.comments, action.payload]
+            })
+            .addCase(deleteComment.fulfilled, (state, action) => {
+                state.comments = state.comments.filter((comment) => comment._id !== action.payload.comment._id)
+            })
+            .addCase(like.fulfilled, (state, action) => {
+                const comments = state.comments.map((element) => {
+                  if (element._id === action.payload._id) {
+                    element = action.payload;
+                  }
+                  return element
+              })
+              state.comments = comments
+              })
+            .addCase(dislike.fulfilled, (state, action) => {
+                const comments = state.comments.map((element) => {
+                  if (element._id === action.payload._id) {
+                    element = action.payload;
+                  }
+                  return element
+              })
+              state.comments = comments
+              })
     }
 });
 
+export const getComments = createAsyncThunk("comment/getComments", async (_id, thunkAPI) => {
+    try {
+      return await commentService.getComments(_id);
+    } catch (error) {
+      const message = error.response.data;
+      return thunkAPI.rejectWithValue(message);
+    }
+  });
 
 export const createComment = createAsyncThunk("comment/create", async (data, thunkApi) => {
     try {
@@ -27,5 +59,32 @@ export const createComment = createAsyncThunk("comment/create", async (data, thu
         return thunkApi.rejectWithValue(message)
     }
 })
+
+export const deleteComment = createAsyncThunk("comment/deleteComment", async (_id, thunkAPI) => {
+    try {
+      return await commentService.deleteComment(_id);
+    } catch (error) {
+      const message = error.response.data;
+      return thunkAPI.rejectWithValue(message);
+    }
+  });
+  
+  export const like = createAsyncThunk("comment/like", async (_id, thunkAPI) => {
+    try {
+      return await commentService.like(_id);
+    } catch (error) {
+      const message = error.response.data;
+      return thunkAPI.rejectWithValue(message);
+    }
+  });
+  
+  export const dislike = createAsyncThunk("comment/dislike", async (_id, thunkAPI) => {
+    try {
+      return await commentService.dislike(_id);
+    } catch (error) {
+      const message = error.response.data;
+      return thunkAPI.rejectWithValue(message);
+    }
+  });
 
 export default commentSlice.reducer;
