97ed4914d1501473a661a00b6a47528d6cff3f76
Gerahara <gerfersie@gmail.com>
2022-03-27 13:12:37 +0200

    Sidebar Admin

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,35 +16,42 @@ const Sidebar = () => {
     }
 
     return (
-        <nav>
-            <div> {user &&
-                <>
-                    <ul>
-                        <li>
-                            <Link to={`/user/profile/${user?.user._id}`}>
-                                {user?.user.name}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={`/user`}>
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/user/forms'>
-                                Forms
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/loginuser' onClick={onLogoutUser}>
-                                Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </>
-            }
-            </div>
-        </nav>
+        <>
+        {
+            user.user.role === 'admin' ?
+                <h1>Hola Admin</h1>
+             :
+                <nav>
+                <div> {user &&
+                    <>
+                        <ul>
+                            <li>
+                                <Link to={`/user/profile/${user?.user._id}`}>
+                                    {user?.user.name}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={`/user`}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/user/forms'>
+                                    Forms
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/loginuser' onClick={onLogoutUser}>
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </>
+                }
+                </div>
+            </nav>       
+        }
+        </>
     )
 }
 
--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import DashboardAdmin from "../components/DashboardAdmin/DashboardAdmin";
+import Sidebar from '../components/Sidebar/Sidebar';
 
 export const AdminRouter = () => {
     return (
+        <>
+        <Sidebar/>
             <Routes>
                 <Route path="/" element={< DashboardAdmin/>} />
             </Routes>
+        </>
             )
 }
