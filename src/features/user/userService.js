2818a19b089cc5b62ff6c2c5d3624f92a7251133
German <gerfersie@gmail.com>
2022-03-29 23:38:44 +0200

    Deploy Heroku 2

--- src/features/user/userService.js
+++ src/features/user/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://tripulacionesback.herokuapp.com";
+const API_URL = "http://localhost:4000";
 
 const registerUser = async (userData) => {
     try {
